af79fcb8ff905e0133bec1fda53202b8fe71cf11
zhaozongchang <xingyingsuifeng@gmail.com>
2019-04-18 17:00:55 +0800

    添加分页供您额

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,11 +5,11 @@ class JobsController < ApplicationController
   def index
     @jobs = case params[:order]
     when 'by_lower_bound'
-      Job.published.order('wage_lower_bound DESC')
+      Job.published.order('wage_lower_bound DESC').paginate(page: params[:page], per_page: 5)
     when 'by_upper_bound'
-      Job.published.order('wage_upper_bound DESC')
+      Job.published.order('wage_upper_bound DESC').paginate(page: params[:page], per_page: 5)
     else
-      Job.published.order('created_at DESC')
+      Job.published.order('created_at DESC').paginate(page: params[:page], per_page: 5)
     end
   end
 
