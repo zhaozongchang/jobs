d058a784846344817c9f562418ae222106ab034d
zhaozongchang <xingyingsuifeng@gmail.com>
2019-04-18 11:52:25 +0800

    添加隐藏按钮

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -35,6 +35,20 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
+  def publish
+    @job = Job.find(params[:id])
+    @job.is_hidden = false
+    @job.save
+    redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+    @job.is_hidden = true
+    @job.save
+    redirect_to :back
+  end
+
   private
 
   def job_params
